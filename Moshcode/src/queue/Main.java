ea72ba0a733b0fdf4c937664647a14f05ac7f0c5
Shawn <wangcong26@hotmail.com>
2019-11-14 09:16:49 -0500

    queue

---
+++ Moshcode/src/queue/Main.java
@@ -0,0 +1,42 @@
+package queue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Queue<Integer> queue = new ArrayDeque<>();
+		queue.add(10);
+		queue.add(20);
+		queue.add(30);
+		// int front = queue.remove();
+		System.out.println(queue);
+		reverse(queue);
+		//System.out.println(queue);
+
+		// reverse a queue
+	}
+
+	public static void reverse(Queue<Integer> queue)
+	{
+		Stack<Integer> stack = new Stack<>();
+
+		while (!queue.isEmpty())
+		{
+			stack.push(queue.remove());
+		}
+		
+		System.out.println(stack);
+		
+		while (!stack.isEmpty())
+		{
+			queue.add(stack.pop());
+		}
+
+		System.out.println(queue);
+	}
+
+}
