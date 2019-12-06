332163270f8f0cbf828143869c4a06bbe6b4bf50
Shawn <wangcong26@hotmail.com>
2019-12-06 16:40:24 -0500

    change back

---
+++ Moshcode/src/hashtable/Main_HashTableShawn.java
@@ -0,0 +1,19 @@
+package hashtable;
+
+public class Main_HashTableShawn
+{
+
+	public static void main(String[] args)
+	{
+		HashTableShawn table = new HashTableShawn();
+		//table.put(6, "A");// 1
+		table.put(8, "B"); // 3
+		table.put(11, "C");
+		//table.put(6, "A+");
+		//table.remove(60);// exception
+		System.out.println("Done");
+		//System.out.println(table.get(6));
+
+	}
+
+}
